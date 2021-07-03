89211d546b97fabbaf7ad0fc3690df58050dcbd5
muhammadfazaaa <mzulfanafidz@gmail.com>
2021-07-03 16:58:15 +0700

    first commit

---
+++ fazagis/data/data1_2.js
@@ -0,0 +1,22 @@
+var json_data1_2 = {
+"type": "FeatureCollection",
+"name": "data1_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 1.0, "Nama Wisata": "Bebek Goreng Pak Thori", "Longitude": 110.3590979, "Latitude": -7.059486 }, "geometry": { "type": "Point", "coordinates": [ 110.3590979, -7.059486 ] } },
+{ "type": "Feature", "properties": { "No": 2.0, "Nama Wisata": "Soto Bangkong", "Longitude": 110.4108002, "Latitude": -7.0621569 }, "geometry": { "type": "Point", "coordinates": [ 110.4108002, -7.0621569 ] } },
+{ "type": "Feature", "properties": { "No": 3.0, "Nama Wisata": "Waroeng Semawis", "Longitude": 110.4242329, "Latitude": -6.9745298 }, "geometry": { "type": "Point", "coordinates": [ 110.4242329, -6.9745298 ] } },
+{ "type": "Feature", "properties": { "No": 4.0, "Nama Wisata": "Kampung Laut", "Longitude": 110.3843915, "Latitude": -6.9624163 }, "geometry": { "type": "Point", "coordinates": [ 110.3843915, -6.9624163 ] } },
+{ "type": "Feature", "properties": { "No": 5.0, "Nama Wisata": "Bakmi Jawa Pak Gareng", "Longitude": 110.4208535, "Latitude": -6.9803593 }, "geometry": { "type": "Point", "coordinates": [ 110.4208535, -6.9803593 ] } },
+{ "type": "Feature", "properties": { "No": 6.0, "Nama Wisata": "Mie Siang Kie", "Longitude": 110.4289554, "Latitude": -6.9811182 }, "geometry": { "type": "Point", "coordinates": [ 110.4289554, -6.9811182 ] } },
+{ "type": "Feature", "properties": { "No": 7.0, "Nama Wisata": "Gulai Kambing Bustaman Pak Sabar", "Longitude": 110.4289554, "Latitude": -6.9935783 }, "geometry": { "type": "Point", "coordinates": [ 110.4289554, -6.9935783 ] } },
+{ "type": "Feature", "properties": { "No": 8.0, "Nama Wisata": "Angkringan Gaul Ampiran 3", "Longitude": 110.3362565, "Latitude": -7.0199323 }, "geometry": { "type": "Point", "coordinates": [ 110.3362565, -7.0199323 ] } },
+{ "type": "Feature", "properties": { "No": 9.0, "Nama Wisata": "Warung Asem-Asem Koh Liem", "Longitude": 110.4246585, "Latitude": -6.9812779 }, "geometry": { "type": "Point", "coordinates": [ 110.4246585, -6.9812779 ] } },
+{ "type": "Feature", "properties": { "No": 10.0, "Nama Wisata": "Soto Kudus Mbak Lin", "Longitude": 110.4255012, "Latitude": -6.9895609 }, "geometry": { "type": "Point", "coordinates": [ 110.4255012, -6.9895609 ] } },
+{ "type": "Feature", "properties": { "No": 11.0, "Nama Wisata": "Bandeng Juwana Elrina", "Longitude": 110.410947, "Latitude": -6.9864351 }, "geometry": { "type": "Point", "coordinates": [ 110.410947, -6.9864351 ] } },
+{ "type": "Feature", "properties": { "No": 12.0, "Nama Wisata": "Kedai Beringin", "Longitude": 110.4125971, "Latitude": -6.9738211 }, "geometry": { "type": "Point", "coordinates": [ 110.4125971, -6.9738211 ] } },
+{ "type": "Feature", "properties": { "No": 13.0, "Nama Wisata": "Lumpia Gang Lombok", "Longitude": 110.4258088, "Latitude": -6.9743621 }, "geometry": { "type": "Point", "coordinates": [ 110.4258088, -6.9743621 ] } },
+{ "type": "Feature", "properties": { "No": 14.0, "Nama Wisata": "Pesta Keboen", "Longitude": 110.4131613, "Latitude": -6.996662 }, "geometry": { "type": "Point", "coordinates": [ 110.4131613, -6.996662 ] } },
+{ "type": "Feature", "properties": { "No": 15.0, "Nama Wisata": "Gulai Kepala Ikan Pak Untung", "Longitude": 110.4292447, "Latitude": -6.9960229 }, "geometry": { "type": "Point", "coordinates": [ 110.4292447, -6.9960229 ] } }
+]
+}
